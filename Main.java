a7757b19f9261aa6be51cd51122f77613abff9db
sandeep <chinnamsettisandeep@gmail.com>
2026-08-19 18:27:55 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,18 +3,19 @@ public class Main
     public static void main (String []args)
     {
         int a = 206456;
-        int d,e=0,o=0;
-        while(a!=0)
+        int d, e = 0, o = 0;
+        
+        while(a != 0)
         {
-        int d = a % 10;
-        if(d % 2 == 0)
-        e++;
-        else 
-        o++;
-        a=a/10;
+            d = a % 10; // Removed "int" since d is already declared above
+            if(d % 2 == 0)
+                e++;
+            else 
+                o++;
+            a = a / 10;
         }
-    System.out.println("ODD: "+o);
-    System.out.println("EVEN: "+e);    
+        
+        System.out.println("ODD: " + o);
+        System.out.println("EVEN: " + e);    
     }
-
 }
